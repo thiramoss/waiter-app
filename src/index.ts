7a23ff2424312af2c3beeb39b6bdbd4d10cf0f48
Thiago Ramos <trs.tiba@gmail.com>
2022-12-26 11:43:46 -0300

    Criando rotas, criando e listando categorias

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 import mongoose from 'mongoose';
 
+import { router } from './router';
+
 
 
 mongoose.connect('mongodb://localhost:27017')
@@ -9,6 +11,10 @@ mongoose.connect('mongodb://localhost:27017')
     const app = express();
 
     const port = 3001;
+
+    app.use(express.json());
+
+    app.use(router);
 
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);

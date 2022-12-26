8eaa8d74abb046999b4f0358c0a838fbb5305186
Thiago Ramos <trs.tiba@gmail.com>
2022-12-26 10:10:27 -0300

    Conectando e modelando o db

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 
-const app = express();
+import mongoose from 'mongoose';
 
-const port = 3002;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+
+    console.log('conectado ao mongo');
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
+
+
